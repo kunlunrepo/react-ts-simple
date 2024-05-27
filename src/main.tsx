c4934fe48235a37600b73510d41cdc77f460f623
kunlunrepo <482976531@qq.com>
2024-05-27 17:23:20 +0800

    BrowserRouter的使用

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function ReactDemo() {
+  return <div>React Demo</div>
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path='/react' element={<ReactDemo />}></Route>
+      </Routes>
+    </React.StrictMode>,
+  </BrowserRouter>
+
 )
