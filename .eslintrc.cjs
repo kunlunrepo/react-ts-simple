ba6232c5c27dd4f81d48d9ce6713a467b5523298
kunlunrepo <482976531@qq.com>
2024-05-27 10:20:10 +0800

    添加插件

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,10 +9,19 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  /**
+   * "off" 或 0    ==>  关闭规则
+   * "warn" 或 1   ==>  打开的规则作为警告（不影响代码执行）
+   * "error" 或 2  ==>  规则作为一个错误（代码不能执行，界面报错）
+   */
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-console': 'error', // 禁止使用console
+    'no-unused-vars': 'error',// 禁止定义未使用的变量
+    'no-debugger': 'error', // 禁止使用debugger
+    'no-var': 'error', // 要求使用 let 或 const 而不是 var
   },
 }
