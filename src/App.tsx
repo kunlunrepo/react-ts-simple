69db4b63568a7607f41982533dc48495d150d0fe
kunlunrepo <482976531@qq.com>
2024-05-27 14:26:15 +0800

    嵌套组件多级传值

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const name = 'Jack'
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>欢迎学习React开发</p>
+        <Child1 name={name} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
+}
+
+function Child1({name} : any) {
+  return (
+    <div>
+      <p><span>Child1</span></p>
+      <p><Child2 name={name}/></p>
+    </div>
+  )
+}
+
+function Child2({name} : any) {
+  return <span>Child2 祖先组件传递的值：{name}</span>
 }
 
 export default App
