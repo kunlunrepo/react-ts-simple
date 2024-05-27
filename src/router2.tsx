5cd8f41cb5fd459579daeaadc0d64ba7e867118b
kunlunrepo <482976531@qq.com>
2024-05-27 18:50:02 +0800

    useActionData的使用

--- src/router2.tsx
+++ src/router2.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, Navigate, Link, useParams, Outlet, redirect, useLoaderData} from "react-router-dom";
+import {createBrowserRouter, Navigate, Link, useParams, Outlet, redirect, useLoaderData, Form, useActionData} from "react-router-dom";
 
 import App from "./App.tsx";
 
@@ -16,6 +16,25 @@ function Test() {
 
 function NotFound() {
   return <div>404 当前页面不存在</div>
+}
+
+function Login() {
+  const errors: any = useActionData()
+  return <Form method="post">
+    <p>
+      <input type="text" name="email" />
+      {errors?.email && <span>{errors.email}</span>}
+    </p>
+
+    <p>
+      <input type="text" name="password" />
+      {errors?.password && <span>{errors.password}</span>}
+    </p>
+
+    <p>
+      <button type="submit">登录</button>
+    </p>
+  </Form>
 }
 
 function Apple() {
@@ -54,6 +73,33 @@ function orderLoader({params}: any) {
   // }
 }
 
+async function loginAction({request}: any) {
+  const formData = await request.formData();
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const errors: any = {
+  };
+
+  // validate the fields
+  if (typeof email !== "string" || !email.includes("@")) {
+    errors.email =
+      "请填入邮箱";
+  }
+
+  if (typeof password !== "string" || password.length < 6) {
+    errors.password = "密码必须大于6位";
+  }
+
+  // return data if we have errors
+  if (Object.keys(errors).length) {
+    return errors;
+  }
+
+  // 条件满足
+  console.log("创建用户成功")
+  return redirect("/");
+}
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -63,6 +109,11 @@ const router = createBrowserRouter([
     path: '/order/:id',
     element: <Order/>,
     loader: orderLoader
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    action: loginAction
   },
   {
     path: '*',
